b815134703f49992a45c631e4d3d9674849d711f
Filip Osowski <filiposowski5@gmail.com>
2018-04-14 14:28:30 -0500

    Add working quit/start through cli along with file management

--- manager.py
+++ manager.py
@@ -1,19 +1,47 @@
 import os
-from scraper import item_scraper
-def start():
-    print("The manager was run.")
-    print("a")
-    try:
-        os.mkfifo(".craigslist_monitor_pipe")
-    except FileExistsError:
-        pass
-    print("b")
-    scrapers = []
-    while True:
-        print("stuck?")
-        with open(".craigslist_monitor_pipe", "r") as pipe:
-            print("something was posted:", pipe.read())
+from daemon import DaemonContext
+from time import sleep
+from sys import exit
+import atexit
 
-    os.remove(".craigslist_monitor_pipe")
+log_file, pid_file, pipe = (None for _ in range(3))
+def main():
+    print("The daemon has started")
+
+    pid_file.write(str(os.getpid()))
+    pid_file.close()
+
+    should_run = True
+
+    while should_run:
+        sleep(1)
+        input = pipe.readline()
+        if input != "":
+            print("Received: " + input)
+            if input == "quit":
+                should_run = False
+    print("Quitting manager...")
+
+
+def create_manager():
+    global log_file, pid_file, pipe
+    log_file = open("log", "w+")
+    log_file.truncate()
+    pid_file = open(".pid", "w+")
+    pipe = os.fdopen(os.open(".craigslist_monitor_pipe", os.O_NONBLOCK | os.O_RDONLY))
+
+    with DaemonContext(files_preserve=[pipe.fileno(), log_file.fileno(), pid_file.fileno()], stdout=log_file, stderr=log_file, working_directory=os.getcwd()):
+        main()
+    
+def clean_up():
     os.remove(".pid")
-start()
+    os.remove(".craigslist_monitor_pipe")
+    pipe.close()
+    log_file.close()
+    print("Successfully exited manager.")
+
+atexit.register(clean_up)
+
+if __name__ == "__main__":
+    create_manager()
+
