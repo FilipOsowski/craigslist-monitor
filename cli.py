f38e1c1d1cc7e3d9d7309d497da1779a37b3a2a8
Filip Osowski <filiposowski5@gmail.com>
2018-04-18 20:50:22 -0500

    Fix bugs and add some documentation

--- cli.py
+++ cli.py
@@ -1,7 +1,12 @@
 import argparse
 
 
+# A decorator that checks for the manager's existence and depending on the
+# result decides whether to run a function.
 def check_manager(should_exist):
+
+    # Checks for the manager's existence by checking for the existence of .pid
+    # file (created by the manager).
     def decorator(function):
         result = None
         try:
@@ -11,59 +16,103 @@ def check_manager(should_exist):
             result = False
 
         def wrapper(*args):
+
+            # If the manager's existence meets expectations, the wrapped
+            # function is run.
             if should_exist == result:
-                print("Expectations met result.")
                 function(*args)
             else:
-                print("Expectations did not meet results.")
+                if result:
+                    print("The manager already exists")
+                else:
+                    print("The manager does not exist.")
 
         return wrapper
     return decorator
 
 
+# Sends a message to the manager specifying the scraper to created. The message
+# includes the specifications for the scraper.
 @check_manager(should_exist=True)
-def add(args):
-    with open(".craigslist_monitor_pipe", "w") as pipe:
-        pipe.write("add " + str(args))
+def add(kwargs):
+    send_to_manager("add " + str(kwargs))
 
 
-def manager(args):
+# Handles all manager related commands
+def manager(kwargs):
+
+    # Handles all manager related commands not having to do with the manager's
+    # creation.
     @check_manager(should_exist=True)
-    def quit_m():
-        print("Quitting manager...")
-        import os.path
-        with open(".craigslist_monitor_pipe", "w") as pipe:
-            pipe.write("quit")
+    def manager_options(option):
 
+        # Tells the manager (and active scrapers) to quit.
+        if option == "quit":
+            send_to_manager("quit")
+
+        # Tells the manager to list active scrapers.
+        elif option == "list":
+            scrapers = send_to_manager("list", print_resp=False)
+            print("Existing scrapers- ", scrapers)
+
+        # Tells the manager to stop a scraper. The message sent includes the
+        # name of the scraper to be stopped.
+        elif option == "stop":
+            send_to_manager(msg="stop " + kwargs["stop"][0])
+
+    # Starts a manager if it doesn't exist by importing the manager module and
+    # calling its start function.
     @check_manager(should_exist=False)
-    def start_m():
-        from os import mkfifo
+    def start_manager():
         from manager import create_manager
-
-        mkfifo(".craigslist_monitor_pipe")
-
-        print("Creating manager...")
+        print("Creating the manager...")
         create_manager()
-    
-    if args["quit"]:
-        quit_m()
-    else:
-        start_m()
 
+    # If a manager command is specified, it is submitted to the manager_options
+    # function. Else, a manager is initialized.
+    for kw in kwargs:
+        if kwargs[kw]:
+            manager_options(kw)
+            break
+    else:
+        start_manager()
+
+
+# Utilizes the socket_tools module to exchange information with the manager.
+# Also closes and keeps track of the client socket.
+def send_to_manager(msg, print_resp=True):
+    import socket_tools
+    client_socket = socket_tools.send(msg)
+    response = socket_tools.receive(client_socket)
+    client_socket.close()
+    
+    if print_resp:
+        print(response)
+    return response
+    
 
 monitor = argparse.ArgumentParser(description="Monitors a craigslist search for new items.")
 monitor_subparsers = monitor.add_subparsers(help="Sub-commands")
 
+# The add interface handles the addition and customization of scrapers.
 add_item = monitor_subparsers.add_parser("add", description="Add an item (craigslist search) to be monitored.")
 add_item.add_argument("name", help="The name of the process.")
-add_item.add_argument("url", help="The url of a (customized) craigslist search to be monitored.")
+add_item.add_argument("monitor", help="The url of a craigslist search or the item name to be monitored.")
+add_item.add_argument("output", help="The file to which items found by the monitor will be written to")
 add_item.add_argument("-r", "--renewals", action="store_true", help="Include old posts that are renewed.")
 add_item.add_argument("-e", "--exclude-words", nargs="+", metavar="word", default=[], help="Exclude posts that contain these words (not case sensative).")
 add_item.set_defaults(func=add)
 
-create_manager = monitor_subparsers.add_parser("manager", description="Starts the manager if issued with no commands.")
-create_manager.add_argument("-q", "--quit", help="Quits the manager.", action="store_true")
-create_manager.set_defaults(func=manager)
-args = vars(monitor.parse_args())
-target_func = args.pop("func")
-target_func(args)
+# The manager interface handles all commands for managing existing scrapers (and the manager itself).
+manager_interface = monitor_subparsers.add_parser("manager", description="Starts the manager if issued with no commands.")
+manager_options = manager_interface.add_mutually_exclusive_group()
+manager_options.add_argument("-q", "--quit", help="Quits the manager and all scrapers", action="store_true")
+manager_options.add_argument("-l", "--list", help="List all the currently running scrapers", action="store_true")
+manager_options.add_argument("-s", "--stop", nargs=1, metavar="scraper_name", help="Stop the named scraper")
+manager_options.set_defaults(func=manager)
+kwargs = vars(monitor.parse_args())
+
+# Removing the "func" argument allows the kwargs to submitted directly to the scraper.
+if kwargs:
+    target_func = kwargs.pop("func")
+    target_func(kwargs)
--- manager.py
+++ manager.py
@@ -1,22 +1,31 @@
 import os
 from daemon import DaemonContext
-from sys import exit
 import atexit
 
+# log_file: The file that the stdout and stderr of the manager is written to.
+# pid_file: Stores the pid of the manager daemon (used for checking the
+# manager's existence)
+# manager_socket: The unix domain socket used for communication between the cli
+# and the manager.
 log_file, pid_file, manager_socket = (None for _ in range(3))
 
 
 def main():
-    import socket
     import socket_tools
     import multiprocessing
-    from time import sleep
     from ast import literal_eval
     from scraper import create_scraper
     print("The manager has started.")
 
+    # The scrapers dictionary keeps track of attributes of active scrapers.
+    # name: The name of the scraper.
+    # process: The scraper's process as returned by multiprocessing.Process 
+    # should_quit: A multiprocessing.Event instance that is used for telling
+    # the scraper when to quit.
     scrapers = {}
 
+    # The scraper's pid is written and the .pid file is closed.
+    # The existence of the .pid file tells the cli that the manager exists.
     pid_file.write(str(os.getpid()))
     pid_file.close()
 
@@ -24,44 +33,75 @@ def main():
 
     while should_run:
 
+        # The manager waits for a connection from the cli. Upon making a
+        # connection, the manager receives the cli's message.
         cli_connection, addr = manager_socket.accept()
         cli_output = socket_tools.receive(cli_connection)
         prefix = cli_output.split()[0]
 
         print("Received: " + cli_output)
+
+        # The manager performs an action depending on the prefix to the cli's
+        # message. The manager also sends a message to the (waiting)
+        # cli_connection socket confirming the action it took.
+
+        # If the manager is ordered to quit, it ends its main loop and sets all
+        # active scrapers' events (signaling them to quit).
         if prefix == "quit":
             should_run = False
             for scraper in scrapers:
                 scrapers[scraper]["should_quit"].set()
-            socket_tools.send(sock=cli_connection, msg="Quitting manager...")
+            socket_tools.send(sock=cli_connection, msg="Quitting the manager...")
+
+        # If the manager is ordered to add a scraper, it checks if the
+        # requirement for adding a scraper is met, and then creates the scraper
+        # and its associated properties.
         elif prefix == "add":
+
+            # The cli's message containing the scraper's specifications is
+            # parsed into a dictionary for later use.
             scraper_kwargs = literal_eval(cli_output[4:])
             scraper_name = scraper_kwargs.pop("name")
 
+            # Checks to see if a scraper_name already exists in the scrapers dictionary.
             if scraper_name in scrapers:
                 socket_tools.send(sock=cli_connection, msg="You can't use the same name for multiple scrapers.")
             else:
+                
+                # Setting the event (later passed to and monitored by the
+                # scraper) tells the scraper when to quit.
                 should_quit = multiprocessing.Event()
                 scraper_kwargs["should_quit"] = should_quit
 
+                # The scraper process is started using the multiprocessing
+                # so as to not block the manager. The kwargs to the manager are
+                # from the cli's message.
                 scraper_process = multiprocessing.Process(target=create_scraper, kwargs=scraper_kwargs)
                 scraper_process.start()
                 scrapers[scraper_name] = {"process": scraper_process, "should_quit": should_quit}
-                print("Started new scraper, name =" scraper_name, "pid =", scraper_process.getpid())
+                print("Started new scraper, name =", scraper_name, ", pid =", scraper_process.pid)
                 socket_tools.send(sock=cli_connection, msg="Successfully added scraper.")
+
+        # If ordered to list, the scraper sends a message to the cli a list
+        # containing the names of currently active scrapers.
         elif prefix == "list":
             socket_tools.send(sock=cli_connection, msg=str([kw for kw in scrapers]))
+
+        # If ordered to stop a scraper, the manager first checks for the
+        # scraper's existence and then signals it to quit.
         elif prefix == "stop":
             scraper_name = cli_output[5:]
             if scraper_name in scrapers:
-                print("Stopping scraper, name =" scraper_name, "pid =", scrapers[scraper_name]["process"].getpid())
+                print("Stopping scraper, name =", scraper_name, ", pid =", scrapers[scraper_name]["process"].pid)
+
+                # The scraper's should_quit event is set, causing the scraper's
+                # main loop to exit.
                 scrapers[scraper_name]["should_quit"].set()
                 socket_tools.send(sock=cli_connection, msg="Successfully stopped scraper named " + scraper_name)
                 del scrapers[scraper_name]
             else:
                 socket_tools.send(sock=cli_connection, msg="There is no scraper named " + scraper_name)
 
-        print("Quitting manager...")
         cli_connection.close()
 
 
@@ -69,6 +109,8 @@ def create_manager():
     import socket
     global log_file, pid_file, manager_socket
 
+    # Creates the unix domain socket for communication with between the
+    # manager and the cli.
     manager_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     manager_socket.bind(".craigslist_monitor_socket")
     manager_socket.listen(1)
@@ -77,9 +119,28 @@ def create_manager():
     log_file.truncate()
     pid_file = open(".pid", "w+")
 
-    with DaemonContext(files_preserve=[manager_socket.fileno(), log_file.fileno(), pid_file.fileno()], stdout=log_file, stderr=log_file, working_directory=os.getcwd()):
+    # Sets up the context for the manager daemon to run in. 
+    daemon_context = DaemonContext()
+
+    # To keep the log_file, manager_socket, and pid_file accessible after
+    # opening the daemon, they are specified to be preserved.
+    daemon_context.files_preserve = [manager_socket.fileno(), log_file.fileno(), pid_file.fileno()] 
+
+    # The manager daemon's stderr and stdout is set to be sent to the log_file.
+    daemon_context.stderr = log_file
+    daemon_context.stdout = log_file
+
+    # For easy access to surrounding files, the manager daemon's working
+    # directory is set to the location of this script.
+    daemon_context.working_directory = os.getcwd()
+
+    # The daemon_context is opened and the manager's main is run in a (now)
+    # daemon process.
+    with daemon_context:
         main()
     
+# Closes and removes resources that are unnecessary when the manager 
+# is not running.
 def clean_up():
     os.remove(".pid")
     os.remove(".craigslist_monitor_socket")
